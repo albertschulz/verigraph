7afbe47348dbfe542f39c6633bf4eca2f5d72fa4
Albert Schulz <albert@mini.speedport.ip>
2020-10-30 14:56:12 +0100

    Prepare levels and root option.

--- verigraph.cpp
+++ verigraph.cpp
@@ -10,15 +10,19 @@
 
 using namespace slang;
 
-void printDepsForInstance(InstanceSymbol const& parent) {
+void printDepsForInstance(InstanceSymbol const& parent, int levels) {
+    if (levels == 0) return;
     for (auto const& child : parent.body.membersOfType<InstanceSymbol>()) {
-        std::cout << "\t\"" << parent.getDefinition().name << "\n" << parent.name << "()\" -> \"" << child.getDefinition().name << "\n" << child.name << "()\"" << std::endl;
-        printDepsForInstance(child);
+        std::cout << "\t\"" << (parent.isInterface()? "<interface>\n":"") << parent.getDefinition().name << "\" -> \"" << (child.isInterface()? "<interface>\n":"") << child.getDefinition().name << "\"" << std::endl;
+        printDepsForInstance(child, levels-1);
     }
 }
 
 int main(int argc, char *argv[])
 {
+    const std::string root = "fifo_fwft_tb";
+    const int levels = 2;
+
     if (argc < 2) {
         std::cerr << "Error: files missing" << std::endl;
         return 1;
@@ -47,8 +51,9 @@ int main(int argc, char *argv[])
     std::cout << "{" << std::endl;
     std::cout << "node [shape=record];" << std::endl;
     if (!topInstances.empty()) {
-        for (auto inst : topInstances) {    
-            printDepsForInstance(*inst);
+        for (auto inst : topInstances) {
+            if (inst->name == root)
+                printDepsForInstance(*inst, levels);
         }
     }
     std::cout << "}" << std::endl;
